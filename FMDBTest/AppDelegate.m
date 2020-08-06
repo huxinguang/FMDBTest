206f984657bfb31cd8aae54e4d6bf417b9d7e03a
huxinguang <1259198268@qq.com>
2020-08-06 18:42:56 +0800

    feature_A_A222

--- FMDBTest/AppDelegate.m
+++ FMDBTest/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    // feature_A add A222
     return YES;
 }
 
